4a3679658974304e84235f8cdb0338fa18446582
qutbiddin <shoislomshobaxromov@gmail.com>
2022-02-11 11:27:56 +0500

    react icons

--- src/App.js
+++ src/App.js
@@ -49,6 +49,12 @@ function App() {
       img: "https://seeklogo.com/images/F/font-awesome-logo-3010FE2434-seeklogo.com.png",
       isChoosen: false,
     },
+    {
+      name: "React Icons",
+      text: "react-icons --save",
+      img: "https://camo.githubusercontent.com/48d099290b4cb2d7937bcd96e8497cf1845b54a810a6432c70cf944b60b40c77/68747470733a2f2f7261776769742e636f6d2f676f72616e67616a69632f72656163742d69636f6e732f6d61737465722f72656163742d69636f6e732e737667",
+      isChoosen: false,
+    },
 
   ];
 
